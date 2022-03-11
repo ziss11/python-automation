39215331ce5942d1deb879a316c011946bc869aa
Abdul Azis <83083246+Aziszy@users.noreply.github.com>
2022-03-11 07:17:43 +0700

    delete useless code

--- Scale and convert images.py
+++ Scale and convert images.py
@@ -5,10 +5,8 @@ import re
 home = os.path.expanduser('~')
 with open(home + '/image_filename.txt') as filename:
     for file in filename.readlines():
-        img = Image.open(home + '/images/' + file.replace('\n', ''))
-
-        filter_filename = re.findall(r'.+', file)
-        filtered_filename = filter_filename[0].replace('.', '')
+        image_filename = file.replace('\n', '')
+        img = Image.open(home + '/images/' + image_filename)
 
         img.rotate(90).resize((128, 128)).convert('RGB').save(
-            '/opt/icons/' + filtered_filename + '.jpg')
+            '/opt/icons/' + image_filename, 'JPEG')
